bb4c96ca6426cb0ce66a5683a84fad2584bc348f
andy_moyle <andy_moyle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-28 15:33:22 +0000

    update to 0.5
    git-svn-id: https://plugins.svn.wordpress.org/emergency-password-reset/trunk@1015350 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -29,14 +29,13 @@ function emergency_password_reset_main()
             $results=$wpdb->get_results('SELECT ID FROM '.$wpdb->prefix.'users');
             if($results){foreach($results AS $row){emergency_password_reset($row->ID);}}
             echo '<h2>All done</h2><p>Please express your relief and appreciation with a coffee donation! <form class="right" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="R7YWSEHFXEU52"><input type="image"  src="https://www.paypal.com/en_GB/i/btn/btn_donate_LG.gif"  name="submit" alt="PayPal - The safer, easier way to pay online."><img alt=""  border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1"></form></p>';
-			echo'<p><a href="http://www.themoyles.co.uk/2013/02/so-your-wordpress-site-has-been-hacked/">Advice on what to do if you think you have been hacked</a></p>';
 			
         }
 		else  if(!empty($_POST['admin_change']) && check_admin_referer('admin_change','admin_change'))
 		{
 			$wpdb->query('UPDATE '.$wpdb->users.' SET user_login="'.esc_sql(stripslashes($_POST['admin'])).'" WHERE user_login="admin"');
 			echo'<div class="updated fade"><p><strong>Admin username reset to "'.stripslashes($_POST['admin']).'"</strong></p></div>';
-			 echo'<p><form action="" method="post">';
+			echo'<p><form action="" method="post">';
             echo wp_nonce_field('emergency_reset','emergency_reset');
             echo'<input type="hidden" name="emergency_accept" value="yes"/><input type="submit" value="Reset all passwords"/></form></p>';
 		}
