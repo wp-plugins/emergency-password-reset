b5b7137d2fae0ce19a8b5006a30cd37bb2f4b145
andy_moyle <andy_moyle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-28 15:28:04 +0000

    Add change username if still have "admin"
    git-svn-id: https://plugins.svn.wordpress.org/emergency-password-reset/trunk@1015348 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  Plugin Name: Emergency password reset
  Plugin URI: http://www.themoyles.co.uk
  Description: Resets all passwords, emailing them to users. <a href="./users.php?page=emergency_password_reset_main">Reset Passwords now</a>
- Version: 0.4
+ Version: 0.5
  Author: andymoyle
  Author URI:http://www.themoyles.co.uk
  */
@@ -29,14 +29,35 @@ function emergency_password_reset_main()
             $results=$wpdb->get_results('SELECT ID FROM '.$wpdb->prefix.'users');
             if($results){foreach($results AS $row){emergency_password_reset($row->ID);}}
             echo '<h2>All done</h2><p>Please express your relief and appreciation with a coffee donation! <form class="right" action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="R7YWSEHFXEU52"><input type="image"  src="https://www.paypal.com/en_GB/i/btn/btn_donate_LG.gif"  name="submit" alt="PayPal - The safer, easier way to pay online."><img alt=""  border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1"></form></p>';
-			echo"<script id='fb67321'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=TheMoyle&button=compact&url='+encodeURIComponent(document.URL);f.title='Flattr';f.height=20;f.width=110;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('fb67321');</script>";
+			echo'<p><a href="http://www.themoyles.co.uk/2013/02/so-your-wordpress-site-has-been-hacked/">Advice on what to do if you think you have been hacked</a></p>';
+			
         }
+		else  if(!empty($_POST['admin_change']) && check_admin_referer('admin_change','admin_change'))
+		{
+			$wpdb->query('UPDATE '.$wpdb->users.' SET user_login="'.esc_sql(stripslashes($_POST['admin'])).'" WHERE user_login="admin"');
+			echo'<div class="updated fade"><p><strong>Admin username reset to "'.stripslashes($_POST['admin']).'"</strong></p></div>';
+			 echo'<p><form action="" method="post">';
+            echo wp_nonce_field('emergency_reset','emergency_reset');
+            echo'<input type="hidden" name="emergency_accept" value="yes"/><input type="submit" value="Reset all passwords"/></form></p>';
+		}
         else
         {
             echo'<p><form action="" method="post">';
             echo wp_nonce_field('emergency_reset','emergency_reset');
             echo'<input type="hidden" name="emergency_accept" value="yes"/><input type="submit" value="Reset all passwords"/></form></p>';
+			
         }
+		$sql='SELECT ID FROM '. $wpdb->users.' WHERE user_login="admin"';
+		
+		$admin=$wpdb->get_var($sql);
+		if(!empty($admin))
+			{
+				echo '<h3> You still have a user called "admin" - that is inviting to be hacked.</h3>';
+				echo'<p><form action="" method="post">';
+				echo wp_nonce_field('admin_change','admin_change');
+				echo'<input type="text" name="admin" placeholder="New admin username"/><input type="submit" value="Change admin username"/></form></p>';
+			
+			}
     }
     else{echo"<p>You don't have permission to use this password reset</p>";}
 }
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.themoyles.co.uk/
 Tags: emergency password reset
 Requires at least: 2.7.0
 Tested up to: 4.0.1
-Stable tag: 0.4
+Stable tag: 0.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -34,9 +34,13 @@ Check out our <a href="http://www.themoyles.co.uk/2013/02/so-your-wordpress-site
 1. The main and only screen!
 
 == Changelog ==
-* 0.1 Initial release
-* 0.2 Correct the title in readme.txt!
+* 0.5 Form to change username from "admin"
+* 0.4 Shows WP 4.0 compatability
 * 0.3 Add Screenshot
+* 0.2 Correct the title in readme.txt!
+* 0.1 Initial release
+
+
 
 == Upgrade notice ==
 * 0.3
